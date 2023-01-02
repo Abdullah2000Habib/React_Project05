703cba548c9fdbd29034a6ea63dd38b41ee030c2
Abdullah2000Habib <abd3habib@gmail.com>
2023-01-02 18:18:45 +0200

    Reasons Component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hero from './Hero/Hero';
 import Program from './Program/Program';
+import Reasons from './Reasons/Reasons';
 
 export default function Home() {
 return (
     <>
         <Hero/>
         <Program />
+        <Reasons />
     </>
 )
 }
