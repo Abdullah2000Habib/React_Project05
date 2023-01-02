fb839b05049b5655f0f847d767b34c76194649ea
Abdullah2000Habib <abd3habib@gmail.com>
2023-01-02 19:41:07 +0200

    Plans and PlanCard Components

---
+++ src/components/Plans/Plans.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import PlanCard from '../PlanCard';
+import { plansData } from './plansData';
+import style from "./style.module.css";
+export default function Plans() {
+return (
+    <>
+        <div className={style.plansContainer}>
+            <div className={style.plansHeader}>
+                <span className='storke-text'>READY TO START</span>
+                <span>YOUR JOURNEY</span>
+                <span  className='storke-text'>NOW WITHUS</span>
+            </div>
+            {/* Plans  Card */}
+            <div className={style.plans}>
+                {plansData.map((item)=>{
+                    return(<PlanCard key={item.id}  icon={item.icon} name={item.name} price={item.price} features={item.features}/>)
+                })}
+            </div>
+        </div>
+    </>
+)
+}
