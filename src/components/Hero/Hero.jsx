612e7cce8bcb77b94a821e62a28f33b9b82a22ea
Abdullah2000Habib <abd3habib@gmail.com>
2023-01-10 14:33:33 +0200

    Responsive for website

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,6 +8,7 @@ import Header from '../Header/Header';
 import style from "./style.module.css";
 export default function Hero() {
   const transition = {type:"spring",duration:3}
+  const mobile = window.innerWidth<=768?true:false;
 return (
   <div>
       <div className={style.hero}>
@@ -17,7 +18,7 @@ return (
           {/* The Best Ad */}
           <div className={style.theBest}>
             <motion.div
-            initial={{left:"238px"}}
+            initial={{left: mobile?"165px":"238px"}}
             whileInView={{left:"8px"}}
             transition={{...transition,type:"tween"}}
             ></motion.div>
